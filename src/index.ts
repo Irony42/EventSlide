79f42092ce3433e8fa17d3b5255d2e1f2a82beb7
Pierre <pierre.tijou@gmail.com>
2023-05-31 17:50:26 +0200

    Add logger and express initialisation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export const toto = (e) => {
-  return e ? "a" : "b"
-}
+import express from 'express'
+import passport from 'passport'
+import { log } from './log'
+import { AddressInfo } from 'net'
+
+const app = express()
+
+const server = app.listen(4300, () => log.debug(`Listening on port ${(server.address() as AddressInfo).port}`))
+
