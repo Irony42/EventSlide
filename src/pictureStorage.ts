2dd4697763302d92fd333f665b53547bef8ed7ee
Pierre <pierre.tijou@gmail.com>
2023-09-05 21:10:34 +0200

    Create pictureStorage.ts

---
+++ src/pictureStorage.ts
@@ -0,0 +1,35 @@
+import fs from 'fs'
+import multer, { Multer } from 'multer'
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const partyName = req.query.partyname
+    if (!partyName) cb(Error('No partyname provided'), '')
+
+    const photoFolder = `photos/${partyName}`
+    if (!fs.existsSync(photoFolder)) fs.mkdirSync(photoFolder, { recursive: true })
+
+    cb(null, photoFolder)
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}_${file.originalname}`)
+  }
+})
+
+const maxSize = 50 * 1024 * 1024 // 50 MB (in bytes)
+
+const fileFilter = (req: any, file: any, cb: any) => {
+  if (file.mimetype.startsWith('image/')) {
+    cb(null, true)
+  } else {
+    cb(new Error('Unsupported file type! Only images are allowed.'))
+  }
+}
+
+export const upload: Multer = multer({
+  storage: storage,
+  fileFilter,
+  limits: {
+    fileSize: maxSize
+  }
+})
